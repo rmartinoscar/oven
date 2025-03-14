e805310e5dbacb0ed8ee09e7a948a76e6f40f421
Joao Patricio <ijpatricio@gmail.com>
2025-03-14 13:10:49 +0000

    App name

--- scripts/filament-issue.sh
+++ scripts/filament-issue.sh
@@ -10,6 +10,7 @@ function create-app-filament-issue-3.x {
 
   # Prepare .env
   cp .env.example .env
+  sed -i 's/^APP_NAME=.*/APP_NAME=Filament/' .env
   php artisan key:generate
 
   # Create SQLite database file
